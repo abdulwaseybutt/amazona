b8f043a62a27d61f10ce7cf76db9dfe54cebdec0
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2024-09-29 11:48:47 +0500

    fixed products table view

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -383,6 +383,12 @@ export default function ProductListScreen(props) {
                       scope="col"
                       className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
                     >
+                      Image
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    >
                       Id
                     </th>
                     <th
